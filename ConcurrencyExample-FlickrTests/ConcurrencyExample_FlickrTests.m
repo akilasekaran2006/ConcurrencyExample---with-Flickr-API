b364afa9f05aa90d893fddedaeb4b1d84506fa0e
Akila Sivapathasekaran <asivapathasekaran@MOBasivapathasekaranMBP15.local>
2013-09-25 10:01:52 -0400

    Initial Commit

---
+++ ConcurrencyExample-FlickrTests/ConcurrencyExample_FlickrTests.m
@@ -0,0 +1,34 @@
+//
+//  ConcurrencyExample_FlickrTests.m
+//  ConcurrencyExample-FlickrTests
+//
+//  Created by Akila Sivapathasekaran on 9/25/13.
+//  Copyright (c) 2013 Akila Sivapathasekaran. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ConcurrencyExample_FlickrTests : XCTestCase
+
+@end
+
+@implementation ConcurrencyExample_FlickrTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
